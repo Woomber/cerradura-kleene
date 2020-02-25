ca1621f291224bbfc45ba7186f49d72860f529e8
Yael Chavoya <yaelchavoya@gmail.com>
2020-02-24 21:32:34 -0600

    Commit inicial

---
+++ NivelCerradura.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CerraduraKleene
+{
+    static class NivelCerradura
+    {
+
+        private static int Nivel = 1;
+        private static List<string> Lenguaje = null;
+
+        public static List<string> Calcular(int nivel, List<string> lenguaje)
+        {
+            if(nivel < 1)
+            {
+                throw new Exception("El nivel debe ser mayor o igual a uno");
+            }
+
+            if (lenguaje == null)
+            {
+                throw new Exception("El lenguaje no puede ser nulo");
+            }
+
+            Nivel = nivel;
+            Lenguaje = lenguaje;
+
+            return CalcularRecursivo(1, lenguaje);
+        }
+
+        private static List<string> CalcularRecursivo(int nivelActual, List<string> lenguajeActual)
+        {
+            if (nivelActual == Nivel)
+            {
+                return lenguajeActual;
+            }
+
+            var nuevoLenguaje = new List<string>();
+
+            foreach(string palabra in Lenguaje)
+            {
+                foreach(string palabra2 in lenguajeActual)
+                {
+                    nuevoLenguaje.Add(palabra + palabra2);
+                }
+            }
+
+            return CalcularRecursivo(nivelActual + 1, nuevoLenguaje);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CerraduraKleene
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numPalabras, nivel;
+            List<string> lenguaje = new List<string>();
+            List<string> nuevoLenguaje;
+
+            Console.WriteLine("Bienvenido al programa de Cerradura de Kleene\nPor Yael Arturo Chavoya Andalón 18310060\n");
+
+            // Leer lenguaje
+            Console.Write("Escriba el número de palabras en el lenguaje:\n> ");
+            numPalabras = int.Parse(Console.ReadLine());
+
+            for (int idx = 1; idx <= numPalabras; idx++)
+            {
+                Console.Write($"Palabra {idx}: ");
+                lenguaje.Add(Console.ReadLine());
+            }
+
+            // Leer nivel
+            Console.Write("\n\nEscriba el nivel que quiere calcular:\n> ");
+            nivel = int.Parse(Console.ReadLine());
+
+            // Cálculo
+            try
+            { 
+                nuevoLenguaje = NivelCerradura.Calcular(nivel, lenguaje);
+            } catch(Exception ex)
+            {
+                Console.WriteLine($"El nivel no se pudo calcular\n{ex.Message}");
+                return;
+            }
+           
+
+            // Mostrar resultado
+            Console.WriteLine($"\n\nEl nivel {nivel} del lenguaje:");
+            ImprimirLenguaje(lenguaje);
+            Console.Write("\n\nes:\n");
+            ImprimirLenguaje(nuevoLenguaje);
+
+            Console.Write("\n\n");
+            Console.WriteLine($"Tiene {nuevoLenguaje.Count} palabras");
+            
+        }
+
+        private static void ImprimirLenguaje(List<string> lenguaje)
+        {
+            foreach (var palabra in lenguaje)
+            {
+                Console.Write(palabra + " ");
+            }
+        }
+
+    }
+}
